359c891752c457a9d2d9039ed627a4d915fa4b21
DuongNguyen <nhatduong30001@gmail.com>
2021-11-01 15:38:48 +0700

    Finish front end

--- src/Tasklist.js
+++ src/Tasklist.js
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./TaskList.css";
 function Tasklist() {
-  const onDeleteClick = () => {
-    console.log("Inside delete");
+  const onDeleteClick = (taskid) => {
+    axios.delete(`http://localhost:4000/deletetask/${taskid}`);
+    getTaskList();
   };
   const onDoneClick = () => {
     console.log("Inside done");
   };
+  const getTaskList = () => {
+    axios
+      .get("http://localhost:4000/task")
+      .then((response) => response.data)
+      .then((response) => setlist(response));
+  };
+  const onSubmitClick = () => {
+    axios.post("http://localhost:4000/addtask", {
+      newtask: task,
+    });
+    getTaskList();
+  };
+  const [list, setlist] = useState([]);
   const [task, settask] = useState("");
+  useEffect(() => {
+    getTaskList();
+    return () => {};
+  }, []);
   return (
     <div className="hero">
       <h4>TaskList</h4>
@@ -17,20 +36,25 @@ function Tasklist() {
           value={task}
           onChange={(e) => settask(e.target.value)}
         />
-        <button>submit</button>
+        <button onClick={() => onSubmitClick()}>submit</button>
       </div>
       <div className="UIcard">
-        <div className="content">
-          <h5>Friends of Veronika</h5>
-          <div className="twobuttons">
-            <button className="button1" onClick={() => onDoneClick()}>
-              Done
-            </button>
-            <button className="button2" onClick={() => onDeleteClick()}>
-              Delete
-            </button>
+        {list.map((task) => (
+          <div className="content">
+            <h5>{task.Name}</h5>
+            <div className="twobuttons">
+              <button className="button1" onClick={() => onDoneClick()}>
+                Done
+              </button>
+              <button
+                className="button2"
+                onClick={() => onDeleteClick(task.TaskID)}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
